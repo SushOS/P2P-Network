df5425a715ae9ba6f2b5076da8c0fa11a7551d85
SushantRavva <ravva.1@iitj.ac.in>
2024-02-13 12:27:07 +0530

    Added function to broadcast with connected peers

--- peer_network.py
+++ peer_network.py
@@ -2,9 +2,13 @@ import socket
 import threading
 import json
 import random
+import time
+import hashlib
 
 MESSAGE_SIZE = 1024
 MAX_PEERS = 4
+MAX_MSG_PER_PEER = 10
+MSG_INTERVAL = 5
 
 class peerNode:
     def __init__(self, p_host, p_port, config_data):
@@ -13,18 +17,21 @@ class peerNode:
         self.p_address = (self.p_host, self.p_port)
         self.chosen_peers = set()
         self.chosen_seeds = []
-        self.config_data = config_data        
+        self.config_data = config_data
+        self.ML = {} # A dictionary of message hashes. As hashes are one to one functions, each hash corresponds to a single value
+        self.msg_cnt = 0 
+        self.timestamp = 0.0
 
     def start(self):
         threading.Thread(target=self.broadcast_msg).start()
         threading.Thread(target=self.check_peer_liveness).start()
     
-    def get_b_half_n(self):
+    def choose_nodes(self):
         n = len(seed_nodes)
         return n // 2
     
     def connect_to_seeds(self):
-        self.chosen_seeds = random.sample(seed_nodes, self.get_b_half_n() + 1)
+        self.chosen_seeds = random.sample(seed_nodes, self.choose_nodes() + 1)
         for s_host, s_port in self.chosen_seeds:
             try:
                 seed_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -64,13 +71,30 @@ class peerNode:
             except Exception as e:
                 print(f"Error connecting to peer {peer_host}:{peer_port}, {e}")
         
-    # def check_peer_liveness(self):
-    #     fail_cnt = 0
-    #     while fail_cnt<3:
+    def gossip_msg(self):
+        self.timestamp = float(time.time())
+        return f"{self.timestamp}:{self.p_host}:{self.msg_cnt+1}"
 
-
-    # def broadcast_msg(self):
-
+    def broadcast_msg(self):
+        while self.msg_cnt < MAX_MSG_PER_PEER: # A node stops after it has generated 10 messages
+            message = self.gossip_msg()
+            message_hash = hashlib.sha256(message.encode()).hexdigest()
+            if message_hash not in self.ML:
+                self.ML[message_hash] = set() # we have created a set as a particular node can send the message to the connected node at most once
+            for (frnd_host, frnd_port) in self.chosen_peers:
+                try:
+                    frnd_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+                    frnd_socket.connect((frnd_host, frnd_port))
+                    frnd_socket.sendall(message.encode())
+                    frnd_socket.close()
+                    self.ML[message_hash].add((frnd_host, frnd_port))
+                    print(f"Message broadcasted to {frnd_host}:{frnd_port}")
+                except Exception as e:
+                    print(f"Failed to broadcast message to {frnd_host}:{frnd_port}")
+            self.msg_cnt+=1
+            # After the node has broadcated a message it needs to stop for 5 seconds to broadcst the next message.
+            time.sleep(MSG_INTERVAL)
+                
 def main():
     with open('./config_file.json') as config_file:
         config_data = json.load(config_file)
